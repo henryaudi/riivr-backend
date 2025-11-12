c52a56f7bd85279ee4e5cac0f2f6cb956a76a1a7
henryaudi <henryperfectest@gmail.com>
2025-11-12 09:06:46 -0700

    Create Redis cache for user auth document

--- src/features/auth/controllers/signup.ts
+++ src/features/auth/controllers/signup.ts
@@ -9,6 +9,11 @@ import { Helpers } from '@global/helpers/helpers';
 import { UploadApiResponse } from 'cloudinary';
 import { uploads } from '@global/helpers/cloudinary-upload';
 import HTTP_STATUS from 'http-status-codes';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { UserCache } from '@service/redis/user.cache';
+import { config } from '@root/config';
+
+const userCache: UserCache = new UserCache();
 
 export class Signup {
   @joiValidation(signupSchema)
@@ -34,10 +39,17 @@ export class Signup {
     });
 
     // Upload avatar image to cloudinary.
-    const result: UploadApiResponse = await uploads(avatarImage, `${userObjectId}`, true, true) as UploadApiResponse;
+    const result: UploadApiResponse = (await uploads(avatarImage, `${userObjectId}`, true, true)) as UploadApiResponse;
     if (!result?.public_id) {
       throw new BadRequestError('File upload: Error occured. Try again.');
     }
+
+    // Add to redis cache.
+    const userDataForCache: IUserDocument = Signup.prototype.userData(authData, userObjectId);
+    userDataForCache.profilePicture =
+      `https://res.cloudinary.com/${config.CLOUD_NAME}/image/upload/v${result.version}/${userObjectId}`;
+    await userCache.saveUserToCache(`${userObjectId}`, uId, userDataForCache);
+
 
     res.status(HTTP_STATUS.CREATED).json({ message: 'User created successfully', authData });
   }
@@ -54,5 +66,42 @@ export class Signup {
       avatarColor,
       createdAt: new Date()
     } as IAuthDocument;
+  }
+
+  private userData(data: IAuthDocument, userObjectId: ObjectId): IUserDocument {
+    const { _id, username, email, uId, password, avatarColor } = data;
+    return {
+      _id: userObjectId,
+      authId: _id,
+      uId,
+      username: Helpers.firstLetterUppercase(username),
+      email,
+      password,
+      avatarColor,
+      profilePicture: '',
+      blocked: [],
+      blockedBy: [],
+      work: '',
+      location: '',
+      school: '',
+      quote: '',
+      bgImageVersion: '',
+      bgImageId: '',
+      followersCount: 0,
+      followingCount: 0,
+      postsCount: 0,
+      notifications: {
+        messages: true,
+        reactions: true,
+        comments: true,
+        follows: true
+      },
+      social: {
+        facebook: '',
+        instagram: '',
+        twitter: '',
+        youtube: ''
+      }
+    } as unknown as IUserDocument;
   }
 }
---
+++ src/shared/services/redis/user.cache.ts
@@ -0,0 +1,102 @@
+import { BaseCache } from '@service/redis/base.cache';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { config } from '@root/config';
+import Logger from 'bunyan';
+import { ServerError } from '@global/helpers/error-handler';
+
+const log: Logger = config.createLogger('userCache');
+
+export class UserCache extends BaseCache {
+  constructor() {
+    super('userCache');
+  }
+
+  public async saveUserToCache(key: string, userUID: string, createdUser: IUserDocument): Promise<void> {
+    const createdAt = new Date();
+    const {
+      _id,
+      uId,
+      username,
+      email,
+      avatarColor,
+      blocked,
+      blockedBy,
+      postsCount,
+      profilePicture,
+      followersCount,
+      followingCount,
+      notifications,
+      work,
+      location,
+      school,
+      quote,
+      bgImageId,
+      bgImageVersion,
+      social
+    } = createdUser;
+
+    const firstList: string[] = [
+      '_id',
+      `${_id}`,
+      'uId',
+      `${uId}`,
+      'username',
+      `${username}`,
+      'email',
+      `${email}`,
+      'avatarColor',
+      `${avatarColor}`,
+      'createdAt',
+      `${createdAt}`,
+      'postsCount',
+      `${postsCount}`,
+    ];
+
+    const secondList: string[] = [
+      'blocked',
+      JSON.stringify(blocked),
+      'blockedBy',
+      JSON.stringify(blockedBy),
+      'profilePicture',
+      `${profilePicture}`,
+      'followersCount',
+      `${followersCount}`,
+      'followingCount',
+      `${followingCount}`,
+      'notifications',
+      JSON.stringify(notifications),
+      'social',
+      JSON.stringify(social),
+    ];
+
+    const thirdList: string[] = [
+      'work',
+      `${work}`,
+      'location',
+      `${location}`,
+      'school',
+      `${school}`,
+      'quote',
+      `${quote}`,
+      'bgImageId',
+      `${bgImageId}`,
+      'bgImageVersion',
+      `${bgImageVersion}`,
+    ];
+
+    const dataToSave: string[] = [...firstList, ...secondList, ...thirdList];
+
+    try {
+      if (!this.client.isOpen) {
+        // Attempting to connect to Redis client.
+        await this.client.connect();
+      }
+
+      await this.client.ZADD('user', { score: parseInt(userUID, 10), value: `${key}` });
+      await this.client.HSET(`users:${key}`, dataToSave);
+    } catch (error) {
+      log.error(`Error saving user to cache: ${error}`);
+      throw new ServerError('Error saving user to cache. Try again!');
+    }
+  }
+}
