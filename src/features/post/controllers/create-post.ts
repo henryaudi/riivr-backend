c7b0e2897a1d70862a465a02dc0fff1bb9556695
henryaudi <henryperfectest@gmail.com>
2025-11-28 09:14:58 -0700

    Add method to create post with image uploads to cloudinary

--- src/features/post/controllers/create-post.ts
+++ src/features/post/controllers/create-post.ts
@@ -1,5 +1,5 @@
 import { joiValidation } from '@global/decorators/joi-validation.decorators';
-import { postSchema } from '@post/validations/post.validation';
+import { postSchema, postWithImageSchema } from '@post/validations/post.validation';
 import { Request, Response } from 'express';
 import { ObjectId } from 'mongodb';
 import HTTP_STATUS from 'http-status-codes';
@@ -7,6 +7,9 @@ import { IPostDocument } from '@post/interfaces/post.interface';
 import { PostCache } from '@service/redis/post.cache';
 import { socketIOPostObject } from '@socket/post';
 import { postQueue } from '@service/queues/post.queue';
+import { UploadApiResponse } from 'cloudinary';
+import { uploads } from '@global/helpers/cloudinary-upload';
+import { BadRequestError } from '@global/helpers/error-handler';
 
 /**
  * Create Post cache instance inside controller to allow redundancy if redis fails
@@ -53,5 +56,55 @@ export class Create {
     postQueue.addPostJob('addPostToDB', { key: req.currentUser!.userId, value: createdPost });
 
     res.status(HTTP_STATUS.CREATED).json({ message: 'Post created successfully', post: createdPost });
+  }
+
+  @joiValidation(postWithImageSchema)
+  public async postWithImage(req: Request, res: Response): Promise<void> {
+    // Added image field (base64 encoded string) in postWithImageSchema.
+    const { post, bgColor, privacy, gifUrl, profilePicture, feelings, image } = req.body;
+
+    /* Upload image to cloudinary */
+    const result: UploadApiResponse = (await uploads(image)) as UploadApiResponse;
+    if (!result?.public_id) {
+      throw new BadRequestError(result.message);
+    }
+
+    const postObjectId: ObjectId = new ObjectId();
+
+    /* Object to be saved to cache and DB */
+    const createdPost: IPostDocument = {
+      _id: postObjectId,
+      userId: req.currentUser!.userId,
+      username: req.currentUser!.username,
+      email: req.currentUser!.email,
+      avatarColor: req.currentUser!.avatarColor,
+      profilePicture,
+      post,
+      bgColor,
+      feelings,
+      privacy,
+      gifUrl,
+      commentsCount: 0,
+      imgVersion: result.version.toString(),
+      imgId: result.public_id,
+      createdAt: new Date(),
+      reactions: { like: 0, love: 0, happy: 0, wow: 0, sad: 0, angry: 0 }
+    } as IPostDocument;
+
+    // Emit socket event to notify clients about the new post.
+    socketIOPostObject.emit('add post', createdPost);
+
+    /* Save to cache and DB */
+    await postCache.savePostToCache({
+      key: postObjectId,
+      currentUserId: `${req.currentUser!.userId}`,
+      uId: `${req.currentUser!.uId}`,
+      createdPost
+    });
+    postQueue.addPostJob('addPostToDB', { key: req.currentUser!.userId, value: createdPost });
+
+    // TODO: create image queue to add image to MongoDB database.
+
+    res.status(HTTP_STATUS.CREATED).json({ message: 'Post created with image successfully', post: createdPost });
   }
 }
