945ba67d79dd770c409f2b3e09601f2f7e72a989
henryaudi <henryperfectest@gmail.com>
2025-11-27 21:24:31 -0700

    Finished post caching integration to the Create class, add test cases

--- src/features/post/controllers/create-post.ts
+++ src/features/post/controllers/create-post.ts
@@ -4,6 +4,13 @@ import { Request, Response } from 'express';
 import { ObjectId } from 'mongodb';
 import HTTP_STATUS from 'http-status-codes';
 import { IPostDocument } from '@post/interfaces/post.interface';
+import { PostCache } from '@service/redis/post.cache';
+
+/**
+ * Create Post cache instance inside controller to allow redundancy if redis fails
+ * (redis connection failure etc.)
+ */
+const postCache: PostCache = new PostCache();
 
 export class Create {
   @joiValidation(postSchema)
@@ -31,6 +38,13 @@ export class Create {
       createdAt: new Date(),
       reactions: { like: 0, love: 0, happy: 0, wow: 0, sad: 0, angry: 0 }
     } as IPostDocument;
+
+    await postCache.savePostToCache({
+      key: postObjectId,
+      currentUserId: `${req.currentUser!.userId}`,
+      uId: `${req.currentUser!.uId}`,
+      createdPost
+    });
 
     res.status(HTTP_STATUS.CREATED).json({ message: 'Post created successfully', post: createdPost });
   }
