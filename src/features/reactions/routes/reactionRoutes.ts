223b743623e2886a1468658c940694b3c3b55b73
henryaudi <henryperfectest@gmail.com>
2025-12-05 08:43:50 -0700

    Add routes for getting reactions

--- src/features/reactions/routes/reactionRoutes.ts
+++ src/features/reactions/routes/reactionRoutes.ts
@@ -1,5 +1,6 @@
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import { Add } from '@reaction/controllers/add-reactions';
+import { Get } from '@reaction/controllers/get-reactions';
 import { Remove } from '@reaction/controllers/remove-reaction';
 import express, { Router } from 'express';
 
@@ -11,9 +12,25 @@ class ReactionRoutes {
   }
 
   public routes(): Router {
+    this.router.get('/post/reactions/:postId', authMiddleware.checkAuthentication, Get.prototype.reactions);
+    this.router.get(
+      'post/single/reaction/username/:username/:postId',
+      authMiddleware.checkAuthentication,
+      Get.prototype.singleReactionByUsername
+    );
+    this.router.get(
+      'post/reactions/username/:username',
+      authMiddleware.checkAuthentication,
+      Get.prototype.reactionsByUsername
+    );
+
     this.router.post('/post/reaction', authMiddleware.checkAuthentication, Add.prototype.reaction);
 
-    this.router.delete('/post/reaction/:postId/:previousReaction/:postReactions', authMiddleware.checkAuthentication, Remove.prototype.reaction);
+    this.router.delete(
+      '/post/reaction/:postId/:previousReaction/:postReactions',
+      authMiddleware.checkAuthentication,
+      Remove.prototype.reaction
+    );
 
     return this.router;
   }
