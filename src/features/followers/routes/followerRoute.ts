b523ddaf6d8860f7f91e5822853f74518ea91ebc
henryaudi <henryperfectest@gmail.com>
2025-12-19 08:59:10 -0700

    Fix caching following key-value not deleted problem

--- src/features/followers/routes/followerRoute.ts
+++ src/features/followers/routes/followerRoute.ts
@@ -1,4 +1,5 @@
 import { Add } from '@follower/controllers/follower-user';
+import { Remove } from '@follower/controllers/unfollow-user';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import express, { Router } from 'express';
 
@@ -11,7 +12,11 @@ class FollowerRoutes {
 
   public routes(): Router {
     this.router.put('/user/follow/:followerId', authMiddleware.checkAuthentication, Add.prototype.follower);
-
+    this.router.put(
+      '/user/unfollow/:followeeId/:followerId',
+      authMiddleware.checkAuthentication,
+      Remove.prototype.follower
+    );
     return this.router;
   }
 }
