bb8964a5751a2b6116b38d914e448a7b925ec0a1
henryaudi <henryperfectest@gmail.com>
2025-12-21 23:59:36 -0700

    Add get follower/following controllers, and the router

--- src/features/followers/routes/followerRoute.ts
+++ src/features/followers/routes/followerRoute.ts
@@ -1,4 +1,5 @@
 import { Add } from '@follower/controllers/follower-user';
+import { Get } from '@follower/controllers/get-followers';
 import { Remove } from '@follower/controllers/unfollow-user';
 import { authMiddleware } from '@global/helpers/auth-middleware';
 import express, { Router } from 'express';
@@ -11,6 +12,9 @@ class FollowerRoutes {
   }
 
   public routes(): Router {
+    this.router.get('/user/following', authMiddleware.checkAuthentication, Get.prototype.userFollowing);
+    this.router.get('/user/followers/:userId', authMiddleware.checkAuthentication, Get.prototype.userFollowers);
+
     this.router.put('/user/follow/:followerId', authMiddleware.checkAuthentication, Add.prototype.follower);
     this.router.put(
       '/user/unfollow/:followeeId/:followerId',
