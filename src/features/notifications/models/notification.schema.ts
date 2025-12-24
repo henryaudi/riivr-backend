8f771564601b333b30c24984fe2dea6e60a3e985
henryaudi <henryperfectest@gmail.com>
2025-12-24 16:31:44 -0700

    Add database service and schema for notifications

--- src/features/notifications/models/notification.schema.ts
+++ src/features/notifications/models/notification.schema.ts
@@ -1,4 +1,5 @@
-import { INotificationDocument } from '@notification/interfaces/notification.interface';
+import { INotification, INotificationDocument } from '@notification/interfaces/notification.interface';
+import { notificationService } from '@service/db/notification.service';
 import mongoose, { model, Model, Schema } from 'mongoose';
 
 const notificationSchema: Schema = new Schema({
@@ -17,6 +18,47 @@ const notificationSchema: Schema = new Schema({
   gifUrl: { type: String, default: '' },
   createdAt: { type: Date, default: Date.now() }
 });
+
+notificationSchema.methods.insertNotification = async function (body: INotification) {
+  const {
+    userTo,
+    userFrom,
+    message,
+    notificationType,
+    entityId,
+    createdItemId,
+    createdAt,
+    comment,
+    reaction,
+    post,
+    imgId,
+    imgVersion,
+    gifUrl
+  } = body;
+
+  await NotificationModel.create({
+    userTo,
+    userFrom,
+    message,
+    notificationType,
+    entityId,
+    createdItemId,
+    createdAt,
+    comment,
+    reaction,
+    post,
+    imgId,
+    imgVersion,
+    gifUrl
+  });
+
+  try {
+    const notifications: INotificationDocument[] = await notificationService.getNotifications(userTo);
+    return notifications;
+  } catch (error) {
+    return error;
+  }
+};
 
 const NotificationModel: Model<INotificationDocument> = model<INotificationDocument>(
   'Notification',
