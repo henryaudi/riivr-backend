a84c66cd6582e9451329965cb5d8aef36448eda4
henryaudi <henryperfectest@gmail.com>
2026-01-01 18:11:24 -0700

    Add add-chat-message controller with email notification and socket IO event broadcasting

---
+++ src/features/chat/controllers/add-chat-message.ts
@@ -0,0 +1,125 @@
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { UserCache } from '@service/redis/user.cache';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { joiValidation } from '@global/decorators/joi-validation.decorators';
+import { addChatSchema } from '@chat/validations/chat';
+import { ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
+import { UploadApiResponse } from 'cloudinary';
+import { BadRequestError } from '@global/helpers/error-handler';
+import { uploads } from '@global/helpers/cloudinary-upload';
+import { config } from '@root/config';
+import { IMessageData, IMessageNotification } from '@chat/interfaces/chat.interface';
+import { socketIOChatObject } from '@socket/chat';
+import { INotificationTemplate } from '@notification/interfaces/notification.interface';
+import { notificationTemplate } from '@service/emails/templates/notifications/notification-template';
+import { emailQueue } from '@service/queues/email.queue';
+
+const userCache: UserCache = new UserCache();
+
+export class Add {
+  @joiValidation(addChatSchema)
+  public async message(req: Request, res: Response): Promise<void> {
+    const {
+      conversationId,
+      receiverId,
+      receiverUsername,
+      receiverAvatarColor,
+      receiverProfilePicture,
+      body,
+      gifUrl,
+      isRead,
+      selectedImage
+    } = req.body;
+    let fileUrl = '';
+    const messageObjectId: ObjectId = new ObjectId();
+    const conversationObjectId: ObjectId = conversationId ? new ObjectId(conversationId as string) : new ObjectId();
+
+    const sender: IUserDocument = (await userCache.getUserFromCache(`${req.currentUser!.userId}`)) as IUserDocument;
+
+    if (selectedImage.length) {
+      const result: UploadApiResponse = (await uploads(
+        req.body.image,
+        `${req.currentUser!.userId}`,
+        true,
+        true
+      )) as UploadApiResponse;
+
+      if (!result?.public_id) {
+        throw new BadRequestError(result.message);
+      }
+
+      fileUrl = `https://res.cloudinary.com/${config.CLOUD_NAME}/image/upload/v${result.version}/${result.public_id}`;
+
+      const messageData: IMessageData = {
+        _id: `${messageObjectId}`,
+        conversationId: new mongoose.Types.ObjectId(conversationObjectId),
+        receiverId,
+        receiverAvatarColor,
+        receiverProfilePicture,
+        receiverUsername,
+        senderUsername: `${req.currentUser!.username}`,
+        senderId: `${req.currentUser!.userId}`,
+        senderAvatarColor: `${req.currentUser!.avatarColor}`,
+        senderProfilePicture: `${sender.profilePicture}`,
+        body,
+        isRead,
+        gifUrl,
+        selectedImage: fileUrl,
+        reaction: [],
+        createdAt: new Date(),
+        deleteForEveryone: false,
+        deleteForMe: false
+      };
+
+      Add.prototype.emitSocketIOEvent(messageData);
+
+      if (!isRead) {
+        // Notify receiver only if the message is unread (i.e., they are not simultaneously in the chat).
+        Add.prototype.messageNotification({
+          currentUser: req.currentUser!,
+          message: body,
+          receiverName: receiverUsername,
+          receiverId,
+          messageData
+        });
+      }
+
+      // TODO: Add sender to chat list in cache
+      // TODO: Add receiver to chat list in cache
+      // TODO: Add message data to cache
+      // TODO: Add message to chat queue
+
+      res.status(HTTP_STATUS.OK).json({ message: 'Message added', conversationId: conversationObjectId });
+    }
+  }
+
+  private emitSocketIOEvent(data: IMessageData): void {
+    socketIOChatObject.emit('message received', data);
+    socketIOChatObject.emit('chat list', data);
+  }
+
+  private async messageNotification({
+    currentUser,
+    message,
+    receiverName,
+    receiverId
+  }: IMessageNotification): Promise<void> {
+    const cachedUser: IUserDocument = (await userCache.getUserFromCache(`${receiverId}`)) as IUserDocument;
+    if (cachedUser.notifications.messages) {
+      // Email notification to receiver turning on their 'messages' settings.
+      const templateParams: INotificationTemplate = {
+        username: receiverName,
+        message,
+        header: `Message notification from ${currentUser.username}`
+      };
+      const template: string = notificationTemplate.notificationMessageTemplate(templateParams);
+      emailQueue.addEmailJob('directMessageEmail', {
+        receiverEmail: cachedUser.email!,
+        template,
+        subject: `You've received messages from ${currentUser.username}`
+      });
+    }
+  }
+}
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -19,6 +19,7 @@ import { SocketIOFollowerHandler } from '@socket/follower';
 import { SocketIOUserHandler } from '@socket/user';
 import { SocketIONotificationHandler } from '@socket/notification';
 import { SocketIOImageHandler } from '@socket/image';
+import { SocketIOChatHandler } from '@socket/chat';
 
 const SERVER_PORT = 5050;
 const log: Logger = config.createLogger('setupServer');
@@ -133,12 +134,14 @@ export class RiivrServer {
     const postSocketHandler: SocketIOPostHandler = new SocketIOPostHandler(io);
     const followerSocketHandler: SocketIOFollowerHandler = new SocketIOFollowerHandler(io);
     const userSocketHandler: SocketIOUserHandler = new SocketIOUserHandler(io);
+    const chatSocketHandler: SocketIOChatHandler = new SocketIOChatHandler(io);
     const notificationSocketHandler: SocketIONotificationHandler = new SocketIONotificationHandler();
     const imageSocketHandler: SocketIOImageHandler = new SocketIOImageHandler();
 
     postSocketHandler.listen();
     followerSocketHandler.listen();
     userSocketHandler.listen();
+    chatSocketHandler.listen();
     notificationSocketHandler.listen(io);
     imageSocketHandler.listen(io);
   }
