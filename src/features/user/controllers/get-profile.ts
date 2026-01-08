9af72c1eba0cae2c450f56e499bcd4d98277c4c5
henryaudi <henryperfectest@gmail.com>
2026-01-08 08:16:50 -0700

    Add controller to get random user, and its route

--- src/features/user/controllers/get-profile.ts
+++ src/features/user/controllers/get-profile.ts
@@ -73,6 +73,20 @@ export class Get {
     res.status(HTTP_STATUS.OK).json({ message: 'Get user profile and posts', user: existingUser, posts: userPosts });
   }
 
+  public async randomUserSuggegstions(req: Request, res: Response): Promise<void> {
+    let randomUsers: IUserDocument[] = [];
+    const cachedUsers: IUserDocument[] = await userCache.getRandomUsersFromCache(`${req.currentUser!.userId}`, req.currentUser!.username);
+
+    if (cachedUsers.length) {
+      randomUsers = [...cachedUsers];
+    } else {
+      const users: IUserDocument[] = await userService.getRandomUsers(req.currentUser!.userId);
+      randomUsers = [...users];
+    }
+
+    res.status(HTTP_STATUS.OK).json({ message: 'User suggestions', users: randomUsers });
+  }
+
   private async allUsers({ newSkip, limit, skip, userId }: IUserAll): Promise<IAllUsers> {
     let users;
     let type = '';
--- src/features/user/routes/userRoutes.ts
+++ src/features/user/routes/userRoutes.ts
@@ -15,6 +15,7 @@ class UserRoutes {
     this.router.get('/user/profile', authMiddleware.checkAuthentication, Get.prototype.profile);
     this.router.get('/user/profile/:userId', authMiddleware.checkAuthentication, Get.prototype.profileByUserId);
     this.router.get('/user/profile/posts/:username/:userId/:uId', authMiddleware.checkAuthentication, Get.prototype.profileAndPosts);
+    this.router.get('/user/profile/user/suggestions', authMiddleware.checkAuthentication, Get.prototype.randomUserSuggegstions);
 
     return this.router;
   }
