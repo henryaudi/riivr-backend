e4379b8fbc4ad440c14e7720e5c612ae4084ae44
henryaudi <henryperfectest@gmail.com>
2026-01-08 00:30:16 -0700

    Add method to get random users from DB

--- src/shared/services/db/user.service.ts
+++ src/shared/services/db/user.service.ts
@@ -1,6 +1,8 @@
 import { IUserDocument } from '@user/interfaces/user.interface';
 import { UserModel } from '@user/models/user.schema';
 import mongoose from 'mongoose';
+import { indexOf } from 'lodash';
+import { followerService } from './follower.service';
 
 class UserService {
   public async addUserData(data: IUserDocument): Promise<void> {
@@ -45,6 +47,41 @@ class UserService {
     ]);
 
     return users;
+  }
+
+  public async getRandomUsers(userId: string): Promise<IUserDocument[]> {
+    const randomUsers: IUserDocument[] = [];
+    const users: IUserDocument[] = await UserModel.aggregate([
+      { $match: { _id: { $ne: new mongoose.Types.ObjectId(userId) } } },
+      { $lookup: { from: 'Auth', localField: 'authId', foreignField: '_id', as: 'authId' } },
+      { $unwind: '$authId' },
+      { $sample: { size: 10 } },
+      {
+        $addFields: {
+          username: '$authId.username',
+          email: '$authId.email',
+          avatarColor: '$authId.avatarColor',
+          uId: '$authId.uId',
+          createdAt: '$authId.createdAt'
+        }
+      },
+      {
+        $project: {
+          authId: 0,
+          __v: 0
+        }
+      }
+    ]);
+
+    const followers: string[] = await followerService.getFolloweesIds(userId);
+    for (const user of users) {
+      const followerIndex = indexOf(followers, `${user._id}`);
+      if (followerIndex < 0) {
+        randomUsers.push(user);
+      }
+    }
+
+    return randomUsers;
   }
 
   private aggregateProject() {
