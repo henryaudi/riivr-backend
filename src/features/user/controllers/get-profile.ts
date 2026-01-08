14af65fe3aca9d2314b0889c9c2f2ddb56f47dba
henryaudi <henryperfectest@gmail.com>
2026-01-08 00:19:39 -0700

    Add method to get random users from cache

--- src/features/user/controllers/get-profile.ts
+++ src/features/user/controllers/get-profile.ts
@@ -8,6 +8,9 @@ import { userService } from '@service/db/user.service';
 import { IFollowerData } from '@follower/interfaces/follower.interface';
 import { followerService } from '@service/db/follower.service';
 import mongoose from 'mongoose';
+import { Helpers } from '@global/helpers/helpers';
+import { IPostDocument } from '@post/interfaces/post.interface';
+import { postService } from '@service/db/post.service';
 
 interface IUserAll {
   newSkip: number;
@@ -52,10 +55,22 @@ export class Get {
   public async profileByUserId(req: Request, res: Response): Promise<void> {
     const { userId } = req.params;
     const cachedUser: IUserDocument = (await userCache.getUserFromCache(userId)) as IUserDocument;
-    const existingUser: IUserDocument = cachedUser
-      ? cachedUser
-      : await userService.getUserById(userId);
+    const existingUser: IUserDocument = cachedUser ? cachedUser : await userService.getUserById(userId);
     res.status(HTTP_STATUS.OK).json({ message: 'Get user profile by id', user: existingUser });
+  }
+
+  public async profileAndPosts(req: Request, res: Response): Promise<void> {
+    const { userId, username, uId } = req.params;
+    const userName: string = Helpers.firstLetterUppercase(username);
+    const cachedUser: IUserDocument = (await userCache.getUserFromCache(userId)) as IUserDocument;
+    const cachedUserPosts: IPostDocument[] = await postCache.getUserPostsFromCache('post', parseInt(uId, 10));
+
+    const existingUser: IUserDocument = cachedUser ? cachedUser : await userService.getUserById(userId);
+    const userPosts: IPostDocument[] = cachedUserPosts.length
+      ? cachedUserPosts
+      : await postService.getPosts({ username: userName }, 0, 100, { createdAt: -1 });
+
+    res.status(HTTP_STATUS.OK).json({ message: 'Get user profile and posts', user: existingUser, posts: userPosts });
   }
 
   private async allUsers({ newSkip, limit, skip, userId }: IUserAll): Promise<IAllUsers> {
--- src/shared/services/redis/user.cache.ts
+++ src/shared/services/redis/user.cache.ts
@@ -2,6 +2,7 @@ import { BaseCache } from '@service/redis/base.cache';
 import { INotificationSettings, ISocialLinks, IUserDocument } from '@user/interfaces/user.interface';
 import { config } from '@root/config';
 import Logger from 'bunyan';
+import { indexOf, findIndex } from 'lodash';
 import { ServerError } from '@global/helpers/error-handler';
 import { Helpers } from '@global/helpers/helpers';
 
@@ -162,6 +163,46 @@ export class UserCache extends BaseCache {
       }
 
       return userReplies;
+    } catch (error) {
+      log.error(`Error getting user from cache: ${error}`);
+      throw new ServerError('Error getting user from cache. Try again!');
+    }
+  }
+
+  public async getRandomUsersFromCache(userId: string, excludedUserName: string): Promise<IUserDocument[]> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const replies: IUserDocument[] = [];
+      const followers: string[] = await this.client.LRANGE(`followers:${userId}`, 0, -1);
+      const users: string[] = await this.client.ZRANGE('user', 0, -1);
+
+      const randomUsers: string[] = Helpers.shuffle(users).slice(0, 10);
+      for (const key of randomUsers) {
+        // Check if the randomly selected user is already follower or the user themself.
+        const followerIndex = indexOf(followers, key);
+        if (followerIndex < 0) {
+          const userHash: IUserDocument = await this.client.HGETALL(`users:${key}`) as unknown as IUserDocument;
+          replies.push(userHash);
+        }
+      }
+      const excludedUsernameIndex: number = findIndex(replies, ['username', excludedUserName]);
+      replies.splice(excludedUsernameIndex, 1);
+      for (const reply of replies) {
+        reply.createdAt = new Date(Helpers.parseJson(`${reply.createdAt}`));
+        reply.postsCount = Helpers.parseJson(`${reply.postsCount}`);
+        reply.blocked = Helpers.parseJson(`${reply.blocked}`);
+        reply.blockedBy = Helpers.parseJson(`${reply.blockedBy}`);
+        reply.notifications = Helpers.parseJson(`${reply.notifications}`);
+        reply.social = Helpers.parseJson(`${reply.social}`);
+        reply.followersCount = Helpers.parseJson(`${reply.followersCount}`);
+        reply.followingCount = Helpers.parseJson(`${reply.followingCount}`);
+        reply.bgImageVersion = Helpers.parseJson(`${reply.bgImageVersion}`);
+        reply.bgImageId = Helpers.parseJson(`${reply.bgImageId}`);
+        reply.profilePicture = Helpers.parseJson(`${reply.profilePicture}`);
+      }
+      return replies;
     } catch (error) {
       log.error(`Error getting user from cache: ${error}`);
       throw new ServerError('Error getting user from cache. Try again!');
