42b235e938b7d3a8235a17b57efec9538304dd3b
henryaudi <henryperfectest@gmail.com>
2026-01-12 07:50:54 -0700

    Add the upload video controller method, routes, and testing http endpoint

--- src/features/post/routes/postRoutes.ts
+++ src/features/post/routes/postRoutes.ts
@@ -18,6 +18,7 @@ class PostRoutes {
 
     this.router.post('/post', authMiddleware.checkAuthentication, Create.prototype.post);
     this.router.post('/post/image/post', authMiddleware.checkAuthentication, Create.prototype.postWithImage);
+    this.router.post('/post/video/post', authMiddleware.checkAuthentication, Create.prototype.postWithVideo);
 
     this.router.put('/post/:postId', authMiddleware.checkAuthentication, Update.prototype.posts);
     this.router.put('/post/image/:postId', authMiddleware.checkAuthentication, Update.prototype.postWithImage);
