7d839ee6a0c69f4656e82bb4da15a7dc483fc485
henryaudi <henryperfectest@gmail.com>
2026-01-11 18:39:17 -0700

    Add health monitoring routes

--- src/features/user/routes/healthRoutes.ts
+++ src/features/user/routes/healthRoutes.ts
@@ -46,6 +46,35 @@ class HealthRoutes {
 
     return this.router;
   }
+
+  public fiboRoutes(): Router {
+    this.router.get('/fibo/:num', async (req: Request, res: Response) => {
+      const { num } = req.params;
+      const start: number = performance.now();
+      const result: number = this.fibo(parseInt(num, 10));
+      const end: number = performance.now();
+      const response = await axios({
+        method: 'get',
+        url: config.EC2_URL
+      });
+      res
+        .status(HTTP_STATUS.OK)
+        .send(
+          `Fibonacci series of ${num} is ${result} and it took ${end - start}ms with EC2 instance of ${
+            response.data
+          } and process id ${process.pid} on ${moment().format('LL')}`
+        );
+    });
+
+    return this.router;
+  }
+
+  private fibo(data: number): number {
+    if (data < 2) {
+      return 1;
+    }
+    return this.fibo(data - 1) + this.fibo(data - 2);
+  }
 }
 
 export const healthRoutes: HealthRoutes = new HealthRoutes();
