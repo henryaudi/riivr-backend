016677640d0de21444e8ba9da068702368acdf1c
henryaudi <henryperfectest@gmail.com>
2026-01-21 09:05:33 -0700

    Remove the comment test

--- infra/1-variables-sample.tf
+++ infra/1-variables-sample.tf
@@ -33,7 +33,6 @@ variable "global_destination_cidr_block" {
   type        = string
   default     = "0.0.0.0/0"
 }
-#test comment line
 
 variable "bastion_host_cidr" {
   description = "CIDR Block for Bastion Host Ingress"
