8e123ca630bf58c69bfdf7e45421582f2d1f07e4
henryaudi <henryperfectest@gmail.com>
2026-01-22 08:06:51 -0700

    Add settings for terraform backend

---
+++ infra/3-main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket  = "riivr-app-terraform-state"
+    key     = "dev/riivr.tfstate"
+    region  = var.aws_region
+    encrypt = true
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    ManagedBy   = "Terraform"
+    Owner       = "Shangjie Zheng"
+  }
+}
