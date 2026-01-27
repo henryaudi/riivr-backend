cb869f0cd8182c3b1a7b245a3ec63a2b73f63b02
henryaudi <henryperfectest@gmail.com>
2026-01-27 08:21:22 -0700

    Add config for eslaticache, and shell to upload .env file

---
+++ infra/scripts/update-env-file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+aws s3 sync s3://riivr-app-terraform-state/dev .
+
+unzip env-file.zip
+
+cp .env.production .env
+em .env.production
+
+sed -i -e "s|\(^REDIS_HOST=\).*|REDIS_HOST=redis://$ELASTICACHE_ENDPOINT_REDIS:6379|g" .env
+
+rm -rf env-file.zip
+cp .env .env.production
+zip env-file.zip .env.production
+
+aws --region us-east-1 s3 cp env-file.zip s3://riivr-app-terraform-state/dev/
+rm -rf .env*
+rm -rf env-file.zip
